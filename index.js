f49333a26ea62e6560a82d65548ef906c4153fda
Asikur <asikurrahman7@gmail.com>
2020-09-26 12:13:40 +0600

    node-mongodb project.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello This is from node js.')
+    res.send('Hello World.')
 });
 
-app.listen(3100, () => console.log('Server open in 3100.'))
+app.listen(3000, () => console.log('Server open in 3000.'))
